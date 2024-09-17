82571b60227f6c7f9a874527ec0a5bcca8d2a6cc
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2024-09-17 12:28:05 +0000

    feat: spring beans

--- src/main/java/com/edson/Project01Application.java
+++ src/main/java/com/edson/Project01Application.java
@@ -1,5 +1,7 @@
 package com.edson;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Project01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Project01Application.class, args);
+		var applicationContext = SpringApplication.run(Project01Application.class, args);
+		System.out.println("==== START BEAN CONTEXT ====");
+		Arrays.stream(applicationContext.getBeanDefinitionNames())
+			.forEach(System.out::println);
+			System.out.println("==== END BEAN CONTEXT ====");
 	}
 
 }
