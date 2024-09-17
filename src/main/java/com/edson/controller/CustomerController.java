ebeabe760722e7b1ecb2af1623b05060428b0d5b
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2024-09-17 14:11:35 +0000

    feat: ResquestParam, required, name

---
+++ src/main/java/com/edson/controller/CustomerController.java
@@ -0,0 +1,29 @@
+package com.edson.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = {"api/v1/customers", "api/v1/customers/"})
+public class CustomerController {
+    public static final List<String> NAMES = List.of("Edson", "Luiz", "Ribeiro", "Rodrigues");
+    
+    @GetMapping
+    public List<String> list() {
+        return NAMES;
+    }
+
+    @GetMapping("filter")
+    public List<String> list(@RequestParam(required = false, name = "name") String pName) {
+        return NAMES.stream().filter(n -> n.equalsIgnoreCase(pName)).toList();
+    }
+
+    @GetMapping("filterlist")
+    public List<String> list(@RequestParam(required = false) List<String> names) {
+        return NAMES.stream().filter(names::contains).toList();
+    }
+}
