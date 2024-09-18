cf8aad24ca2acc2eebeee9dd6589032824a60968
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2024-09-18 18:49:53 +0000

    feat: PostMapping

--- src/main/java/com/edson/controller/HelloController.java
+++ src/main/java/com/edson/controller/HelloController.java
@@ -1,14 +1,27 @@
 package com.edson.controller;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j2;
+
 @RestController
 @RequestMapping("greetings")
+@Log4j2
 public class HelloController {
     @GetMapping("hi")
     public String hi() {
         return "hi";
+    }
+
+    @PostMapping
+    public Long save(@RequestBody String pName) {
+        log.info("Saving name '{}'", pName);
+        return ThreadLocalRandom.current().nextLong();
     }
 }
